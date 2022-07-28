5d45a8cf1cbcf431d23f8ff967883f5a18ac543f
Dima Kobzar <kobzar7dima@gmail.com>
2022-07-28 12:05:42 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#nav-menu') {
-    document.body.classList.add('page__body--scroll-remove');
-  } else {
-    document.body.classList.remove('page__body--scroll-remove');
-  }
-});
